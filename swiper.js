f1761255adafd447c68e8c2e498195f0d25e97b9
iosif-dev <i.lelevkin@gmail.com>
2024-10-11 14:51:51 +0600

    strange swiper

---
+++ swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.news-card-large__slider', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
