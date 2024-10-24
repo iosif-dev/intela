9b8bfec531c7e257889fec920bd4921918ce32e1
iosif-dev <i.lelevkin@gmail.com>
2024-10-24 15:45:02 +0600

    baza

---
+++ swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.news-card-large__slider', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
